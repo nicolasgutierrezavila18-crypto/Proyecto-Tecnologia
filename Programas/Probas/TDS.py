ead4a2ce04b194f835e43dc1c4bd8221f8ba14c0
Nicolás Gutiérrez Ávila <nicolasgutierrezavila18@gmail.com>
2026-02-24 12:26:08 +0100

    Update TDS.py

--- Programas/Probas/TDS.py
+++ Programas/Probas/TDS.py
@@ -1,2 +1,2 @@
 '''Programa para la funcion del sensor TDS              Nicolas Gutierrez Ávila       24/02/26
-para el Proyecto de tecnologia IES de Teis         
+para el Proyecto de tecnologia IES de Teis'''         
