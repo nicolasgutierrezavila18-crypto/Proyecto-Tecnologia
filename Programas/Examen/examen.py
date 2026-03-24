67ad5b0c278983b62d7b8bd719b56eb4dce2667c
Nicolás Gutiérrez Ávila <nicolasgutierrezavila18@gmail.com>
2026-03-24 12:28:19 +0100

    Update examen.py

--- Programas/Examen/examen.py
+++ Programas/Examen/examen.py
@@ -1,1 +1,42 @@
 '''Proba de Evaliacion Tecnoloxia Nicolás Gutiérrez Ávila'''
+#include "ABlocks_DHT.h"
+#include <ESP32Servo.h>
+
+double Temperatura;
+double Humedad;
+DHT dht23(23,DHT11);
+Servo servo_18;
+
+void setup()
+{
+  	pinMode(23, INPUT);
+	servo_18.attach(18);
+
+	Serial.begin(115200);
+	Serial.flush();
+	while(Serial.available()>0)Serial.read();
+
+	dht23.begin();
+
+}
+
+
+void loop()
+{
+	yield();
+
+  	Temperatura = dht23.readTemperature();
+  	Humedad = dht23.readHumidity();
+  	Serial.println(String("Temperatura")+String(Temperatura));
+  	Serial.println(String("Humedad")+String(Humedad));
+  	servo_18.write(3);
+  	if (((Temperatura >= 22) || (Humedad >= 60))) {
+  		servo_18.write(90);
+  	}
+  	else {
+  		servo_18.write(3);
+  	}
+
+  	delay(2000);
+
+}
